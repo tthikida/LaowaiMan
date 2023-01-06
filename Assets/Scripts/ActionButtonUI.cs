cd48d16b41bac70abf78a155662e442bcbafd7a7
tamioTech <62820237+tamioTech@users.noreply.github.com>
2023-01-06 15:11:33 -0800

    addListener working

--- Assets/Scripts/ActionButtonUI.cs
+++ Assets/Scripts/ActionButtonUI.cs
@@ -7,10 +7,27 @@ using TMPro;
 public class ActionButtonUI : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI textMeshPro;
-    [SerializeField] private ActionButtonUI button;
+    [SerializeField] private Button button;
+
 
     public void SetBaseAction(BaseAction baseAction)
     {
+        print("setBaseAction");
+        
         textMeshPro.text = baseAction.GetActionName().ToUpper();
+
+
+        button.onClick.AddListener(() => {
+            print("button clicked");
+            UnitActionSystem.Instance.SetSelectedAction(baseAction);
+            print(baseAction);
+        });
+
+        //button.onClick.AddListener(MoveActionBtn_Click);
+    }
+
+    private void MoveActionBtn_Click()
+    {
+        print("clicked");
     }
 }
--- Assets/Scripts/UnitActionSystem.cs
+++ Assets/Scripts/UnitActionSystem.cs
@@ -12,6 +12,7 @@ public class UnitActionSystem : MonoBehaviour
     [SerializeField] private Unit selectedUnit;
     [SerializeField] private LayerMask unitLayerMask;
 
+    private BaseAction selectedAction;
     private bool isBusy;
 
     private void Awake()
@@ -25,9 +26,15 @@ public class UnitActionSystem : MonoBehaviour
         Instance = this;
     }
 
+    private void Start()
+    {
+        SetSelectedUnit(selectedUnit);
+    }
+
     private void Update()
     {
         if (isBusy) { return; }
+
         if (Input.GetMouseButtonDown(0))
         {
             if (TryHandleUnitSelection()) return;
@@ -76,7 +83,13 @@ public class UnitActionSystem : MonoBehaviour
     private void SetSelectedUnit(Unit unit)
     {
         selectedUnit = unit;
+        SetSelectedAction(unit.GetMoveAction()) ;
         OnSelectedUnitChanged?.Invoke(this, EventArgs.Empty);
+    }
+
+    public void SetSelectedAction(BaseAction baseAction)
+    {
+        selectedAction = baseAction;
     }
 
     public Unit GetSeletedUnit()
